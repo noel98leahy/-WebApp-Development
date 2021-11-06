a5b0566c14bc08475a05860db70424838df03d54
Noel Leahy <nleahy1234@gmail.com>
2021-11-06 17:51:34 +0000

    Arrays.

---
+++ lab001/fundamentals.js
@@ -0,0 +1,40 @@
+const aCar = {
+  owner: "Joe Bloggs",
+  previous_owners: [
+    { name: "Pat Smith", address: "1 Main Street!"},
+    { name: "Andy Fortune", address: "2 Main Street!"}
+  ],
+  type: {
+    make: "Toyota",
+    model: "Corolla",
+    cc: 1.8
+  },
+  registration: { year: 201, county: "WD", number: 1058 }
+};
+
+console.log(aCar.owner + " drives a " + aCar.type.make);
+console.log(
+  "Reg. = " +
+    aCar.registration.year +
+    "-" +
+    aCar.registration.county +
+    "-" +
+    aCar.registration.number
+);
+aCar.milage = 10000
+aCar.color = {
+  exterior: "red",
+  interior: { texture: "leather", shade: "cream" }
+}
+
+console.log(
+  "It is a " +
+    aCar.color.exterior +
+    " car, " +
+    aCar.milage +
+    " milage, with " +
+    aCar.color.interior.texture +
+    " interior."
+);
+
+console.log('First owner : ' + aCar.previous_owners[0].name + '-' + aCar.previous_owners[0].address);
